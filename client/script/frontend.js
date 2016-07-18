5aab8afa6d126eda2cb13131889e3ddbc7e6b486
Pfeter <peter.foldvary@gmail.com>
2016-07-18 20:57:50 +0200

    Add meal frontend - Create a form for adding a new meal.

--- client/script/frontend.js
+++ client/script/frontend.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const url = 'http://localhost:3000/meals';
+
+function getNowDatetime() {
+  var mealTimeField = document.getElementById('meal-time');
+  var now = new Date();
+  // mealTimeField.valueAsNumber = now.getTime();
+  mealTimeField.valueAsDate = "2016.05.18 16:30";
+}
+
+getNowDatetime();
+
+function addMeal() {
+  const xhr = new XMLHttpRequest();
+  const inputMeal = document.querySelector('#input-meal').value;
+  const inputCalorie = document.querySelector('#input-calorie').value;
+  const inputMealTime = document.querySelector('#meal-time').value;
+  const inputValues = { name: inputMeal, calorie: inputCalorie, date: inputMealTime };
+  xhr.open('POST', url);
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.send(JSON.stringify(inputValues));
+}
+
+document.querySelector('.add-button').addEventListener('click', function () {
+  addMeal();
+});
